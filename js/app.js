e203e123745d860bda35f5781d1da5721950d41c
Wonyoung Choi <won@Wonyoungs-MacBook.local>
2020-01-23 11:23:17 +0900

    fixed

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@
 const container = document.querySelector(".map");
 const main = document.querySelector(".map-container");
 
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
 window.addEventListener("resize", () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
